39b3d196a7488d1f669aaae2bb532b66f943c139
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-02-20 22:39:51 -0300

    add: progress bar lib

--- src/script.sh
+++ src/script.sh
@@ -1,34 +1,46 @@
 #!/usr/bin/env bash
+clean_line() { printf "\r"; }
+set_color_progress() {
+    if (( $1 < 25 )); then
+        tput setaf 1
+    elif (( $1 < 50 )); then
+        tput setaf 3
+    elif (( $1 < 75 )); then
+        tput setaf 3
+    else
+        tput setaf 4
+    fi
+}
 resultado=0
 minimo=10000000000
 minimo_atual=0
 semente=0
-limit=25
+limit=10
 make
-source progress-bar.sh/progress-bar.sh
+source libs/progress-bar.sh/progress-bar.sh
+clear
+echo -e "Buscando...\n"
+tput civis
+vertical_center=$((($(tput lines) - 3 )/ 2))
+for i in $(seq 1 $vertical_center); do
+    printf "\n"
+done
 for i in $(seq 1 $limit); do
-    if [ $(expr $i % 3) -eq 0 ]; then
-        echo "Procurando."
-    fi
-    if [ $(expr $i % 3) -eq 1 ]; then
-        echo "Procurando.."
-    fi
-    if [ $(expr $i % 3) -eq 2 ]; then
-        echo "Procurando..."
-    fi
-    
-    progress-bar $i
+    set_color_progress $((i*100/limit))
+    progress-bar $i $limit
+
     resultado=$(./evol)
     semente_atual=$(echo $resultado | grep Semente | cut -d' ' -f2)
     minimo_atual=$(echo $resultado | grep Fitness | cut -d' ' -f15)
     if (( $(echo "$minimo > $minimo_atual" | bc -l) )); then
-        echo "Novo minimo"
-        echo $minimo_atual
         minimo=$minimo_atual
         semente=$semente_atual
     fi
-    clear
-done;
+    printf "\r";
 
+done;
+tput reset
+tput setaf 2
+echo -e "\nResultado:\n"
 echo "Semente: $semente"
 echo "Fitness: $minimo"

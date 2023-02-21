9ddafc25ef5f9b598febc1ad19f75f6fbc69326b
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-02-20 22:58:40 -0300

    add: getopts in search-min.sh

--- src/search-min.sh
+++ src/search-min.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+
+# Options
+limit=10
+usage() { echo "Usage: $0 [-n <Numero de execuções>] [-e <Tempo de cada execução>]" 1>&2; exit 1; }
+
+while getopts ":n:e:" o; do
+    case "${o}" in
+        n)
+            limit=${OPTARG}
+            ;;
+        e)
+            p=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+# Main code
+
 clean_line() { printf "\r"; }
 set_color_progress() {
     if (( $1 < 25 )); then
@@ -11,20 +34,23 @@ set_color_progress() {
         tput setaf 4
     fi
 }
+
+make
+clear
+source libs/progress-bar.sh/progress-bar.sh
+echo -e "Buscando em ${limit} execuções...\n"
+tput civis
+
 resultado=0
 minimo=10000000000
 minimo_atual=0
 semente=0
-limit=10
-make
-source libs/progress-bar.sh/progress-bar.sh
-clear
-echo -e "Buscando...\n"
-tput civis
+
 vertical_center=$((($(tput lines) - 3 )/ 2))
 for i in $(seq 1 $vertical_center); do
     printf "\n"
 done
+
 for i in $(seq 1 $limit); do
     set_color_progress $((i*100/limit))
     progress-bar $i $limit
